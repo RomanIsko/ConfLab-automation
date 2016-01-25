fcf96db9714882f1acd7ef54f31d486a2e9d496a
Roman Isko <iskoroman@gmail.com>
2016-01-25 13:03:50 +0200

    start new browser profile instead sign out

--- src/test/java/com/intelliarts/conflab/automation/web/login/RequireLogin.java
+++ src/test/java/com/intelliarts/conflab/automation/web/login/RequireLogin.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.close;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.sleep;
 import static com.codeborne.selenide.WebDriverRunner.url;
@@ -37,7 +38,7 @@ public class RequireLogin extends BasicTestCase {
         ElementsCollection eventsList = $$(".list-group-item");
         eventsList.last().click();
         eventUrl = url();
-        logout();
+        close();
     }
 
     @AfterClass

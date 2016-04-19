a3bb2c8c7a83335ecd1096b2475928eac1c3d2f4
Roman Isko <iskoroman@gmail.com>
2016-04-19 13:09:11 +0300

    fix Info form locator

--- src/main/java/com/intelliarts/conflab/utils/ElementLocatorData.java
+++ src/main/java/com/intelliarts/conflab/utils/ElementLocatorData.java
@@ -20,7 +20,7 @@ public class ElementLocatorData {
     public static final By EVENT_EDIT_REGION       = By.id("edit-region");
     public static final By EVENT_TITLE             = By.id("page-title");
     public static final By EVENT_TABS              = By.cssSelector(".nav.nav-tabs");
-    public static final By INFO_FORM               = By.xpath("html/body/div[1]/div/div[2]/div/div[2]");
+    public static final By INFO_FORM               = By.xpath("html/body/div[1]/div/div[2]/div/div[1]/div/address");
     public static final By FORM_EDIT_BUTTON        = By.cssSelector(".glyphicon.glyphicon-pencil.edit");
     public static final By INFO_TAB                = By.xpath("html/body/div[1]/div/div[1]/ul/li[1]");
     public static final By SPEAKERS_TAB            = By.xpath("html/body/div[1]/div/div[1]/ul/li[2]");

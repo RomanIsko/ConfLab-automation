6e580288c6bbe081c8cf3c671119c01e0be0638c
Roman Isko <iskoroman@gmail.com>
2016-05-10 13:05:29 +0300

    use random values for create an event

--- src/test/java/com/intelliarts/conflab/automation/web/event/SingleEventViewInfoTab.java
+++ src/test/java/com/intelliarts/conflab/automation/web/event/SingleEventViewInfoTab.java
@@ -3,6 +3,10 @@ package com.intelliarts.conflab.automation.web.event;
 import com.codeborne.selenide.Selectors;
 import com.intelliarts.conflab.automation.web.BasicTestCase;
 import com.intelliarts.conflab.utils.ConfLabEvent;
+import io.codearte.jfairy.Fairy;
+import io.codearte.jfairy.producer.person.Address;
+import io.codearte.jfairy.producer.person.Person;
+import io.codearte.jfairy.producer.text.TextProducer;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -22,25 +26,24 @@ import static com.intelliarts.conflab.utils.ElementLocatorData.FORM_EDIT_BUTTON;
 import static com.intelliarts.conflab.utils.ElementLocatorData.INFO_FORM;
 
 public class SingleEventViewInfoTab extends BasicTestCase {
-    protected static String eventDescription =
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam iaculis, massa ut viverra accumsan, nunc" +
-            " massa rutrum orci, eu pulvinar felis dui non purus. Etiam vel sem ac mi dapibus tempus a tempus erat. " +
-            "Sed ut diam iaculis, sodales ipsum et, vehicula tortor. Donec aliquam felis at lorem tincidunt malesuada" +
-            ". Ut sagittis justo turpis, sed laoreet libero mollis non. Vestibulum non malesuada eros. Duis lacinia " +
-            "ex et justo aliquam porta. In finibus nulla lorem, id vulputate lectus sodales id. Pellentesque " +
-            "ullamcorper massa nec maximus laoreet. Proin sit amet est neque. Etiam placerat massa vel porta pharetra" +
-            ". Aenean sollicitudin leo orci, vitae tristique mi commodo et. Nunc commodo, felis vel finibus " +
-            "ultricies, nisi lorem mollis lacus, eget dictum ligula tellus sit amet nisl. Quisque iaculis consequat " +
-            "augue, a ultricies quam viverra a. Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-    protected static String eventStartDate   = "03-Jan-2016";
-    protected static String eventEndDate     = "05-Jan-2016";
-    protected static String eventCountry     = "Switzerland";
-    protected static String eventCity        = "Zürich";
-    protected static String eventAddress     = "Neugasse 57-63, 8005";
-    protected static String eventContacts    = "+41 44 444 44 44\n" +
-                                               "John Smith\n" +
-                                               "skype:  john.smith\n" +
-                                               "mailto: john-100.smith-500@email.com";
+    private static final TextProducer textProducer = Fairy.create().textProducer();
+    private static final Person       person       = Fairy.create().person();
+    private static final Address      address      = person.getAddress();
+
+    private static final String eventDescription = textProducer.paragraph(3);
+    private static final String eventStartDate   = "03-Jan-2016";
+    private static final String eventEndDate     = "05-Jan-2016";
+    private static final String eventCountry     = "Switzerland";
+    private static final String eventCity        = address.getCity();
+    private static final String eventAddress     = address.street() + " " + address.streetNumber();
+    private static       String email            = person.email();
+    private static       String username         = person.username();
+    private static       String telephoneNumber  = person.telephoneNumber();
+    private static       String fullName         = person.fullName();
+    private static final String eventContacts    = telephoneNumber + "\n" +
+                                                   fullName + "\n" +
+                                                   "skype:  " + username + "\n" +
+                                                   "mailto: " + email;
 
     @BeforeClass
     public static void setUp() throws Exception {
